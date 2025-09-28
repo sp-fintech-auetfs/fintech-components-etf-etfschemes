11707d73b05b219ca25ea7c32fda2bcd85cabfa7
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-09-29 03:19:55 +0530

    update

--- Widgets.php
+++ Widgets.php
@@ -10,29 +10,31 @@ class Widgets extends ComponentsWidgets
 {
     public function etfWatchlist($widget, $dashboardWidget)
     {
-        $schemePackage = new EtfSchemes;
+        if (isset($dashboardWidget['getWidgetData'])) {
+            $schemePackage = new EtfSchemes;
 
-        $watchlists = $schemePackage->getWatchlistByAccountId();
+            $watchlists = $schemePackage->getWatchlistByAccountId();
 
-        $watchlistsSchemes = [];
+            $watchlistsSchemes = [];
 
-        if ($watchlists && count($watchlists['schemes']) > 0) {
-            $schemePackage->switchModel();
+            if ($watchlists && count($watchlists['schemes']) > 0) {
+                $schemePackage->switchModel();
 
-            foreach ($watchlists['schemes'] as $schemeId) {
-                $schemeArr = $schemePackage->getSchemeById((int) $schemeId, false, false, false, false);
+                foreach ($watchlists['schemes'] as $schemeId) {
+                    $schemeArr = $schemePackage->getSchemeById((int) $schemeId, false, false, false, false);
 
-                if ($schemeArr) {
-                    $watchlistsSchemes['schemes'][$schemeArr['id']] = [];
-                    $watchlistsSchemes['schemes'][$schemeArr['id']]['symbol'] = $schemeArr['symbol'];
-                    $watchlistsSchemes['schemes'][$schemeArr['id']]['name'] = $schemeArr['name'];
-                    $watchlistsSchemes['schemes'][$schemeArr['id']]['latest_nav'] = round($schemeArr['latest_nav'], 2);
-                    $watchlistsSchemes['schemes'][$schemeArr['id']]['day_cagr'] = $schemeArr['day_cagr'];
+                    if ($schemeArr) {
+                        $watchlistsSchemes['schemes'][$schemeArr['id']] = [];
+                        $watchlistsSchemes['schemes'][$schemeArr['id']]['symbol'] = $schemeArr['symbol'];
+                        $watchlistsSchemes['schemes'][$schemeArr['id']]['name'] = $schemeArr['name'];
+                        $watchlistsSchemes['schemes'][$schemeArr['id']]['latest_nav'] = round($schemeArr['latest_nav'], 2);
+                        $watchlistsSchemes['schemes'][$schemeArr['id']]['day_cagr'] = $schemeArr['day_cagr'];
+                    }
                 }
             }
-        }
 
-        $widget['data']['settings']['watchlist'] = $watchlistsSchemes;
+            $widget['data']['settings']['watchlist'] = $watchlistsSchemes;
+        }
 
         return $this->getWidgetContent($widget, $dashboardWidget);
     }
